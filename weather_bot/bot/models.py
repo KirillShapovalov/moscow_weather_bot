fd2dabe747d439e6da38dd25842a96debe516fe0
kirill <kirill.shapovlov@gmail.com>
2022-04-23 22:52:20 +0300

    lil fixes

--- weather_bot/bot/models.py
+++ weather_bot/bot/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Users(models.Model):
+
+    login = models.CharField(max_length=50)
+    chat_id = models.IntegerField()
+    password = models.CharField(max_length=20)
--- weather_bot/weather_bot/settings.py
+++ weather_bot/weather_bot/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -83,7 +83,7 @@ DATABASES = {
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': 'db',
-        'PORT': '5439',
+        'PORT': '5432',
     }
 }
 
